bc093a5271d38c9ea0a1c5d8a3b3ac3fa41e2996
Alex Krassikov <a.krassikov@gmail.com>
2011-12-07 02:28:46 -0800

    Added csv upload functional tests for grade_entry_forms_controller. Fixed an error I made in an ealier commit

--- app/models/grade_entry_form.rb
+++ app/models/grade_entry_form.rb
@@ -238,12 +238,13 @@ class GradeEntryForm < ActiveRecord::Base
     num_lines_read = 0
     names = []
     totals = []
+    grades_file = StringIO.new(grades_file.read)
     if encoding != nil
       grades_file = StringIO.new(Iconv.iconv('UTF-8', encoding, grades_file.read).join)
     end
 
     # Parse the question names
-    FasterCSV.parse(grades_file.read) do |row|
+    FasterCSV.parse(grades_file.readline) do |row|
       if !FasterCSV.generate_line(row).strip.empty?
         names = row
         num_lines_read += 1
@@ -251,7 +252,7 @@ class GradeEntryForm < ActiveRecord::Base
     end
 
     # Parse the question totals
-    FasterCSV.parse(grades_file.read) do |row|
+    FasterCSV.parse(grades_file.readline) do |row|
       if !FasterCSV.generate_line(row).strip.empty?
         totals = row
         num_lines_read += 1
